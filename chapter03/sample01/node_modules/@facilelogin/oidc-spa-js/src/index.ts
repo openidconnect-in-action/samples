8939bb53d4ac154778feb3b5a5531b803e790de2
Prabath Siriwardana <prabathsiriwardana@Prabaths-MacBook-Pro.local>
2021-01-25 05:33:29 -0800

    updates

---
+++ chapter03/sample01/node_modules/@facilelogin/oidc-spa-js/src/index.ts
@@ -0,0 +1,24 @@
+import 'core-js/es/string/starts-with';
+import 'core-js/es/array/from';
+import 'core-js/es/typed-array/slice';
+import 'core-js/es/array/includes';
+import 'core-js/es/string/includes';
+import 'core-js/es/set';
+import 'promise-polyfill/src/polyfill';
+import 'fast-text-encoding';
+import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
+
+import Auth0Client from './Auth0Client';
+import { Auth0ClientOptions } from './global';
+
+import './global';
+
+export * from './global';
+
+export default async function createAuth0Client(options: Auth0ClientOptions) {
+  const auth0 = new Auth0Client(options);
+  await auth0.checkSession();
+  return auth0;
+}
+
+export { Auth0Client };
